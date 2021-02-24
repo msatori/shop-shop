8bae5ff2ae0a78e75d9188914e5d41897b758221
Maya Santiago <msatori.code@gmail.com>
2021-02-24 13:10:14 -0600

    stripe fully functional

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from '@stripe/stripe-js';
+import { idbPromise } from "../../utils/helpers"
+import Auth from "../../utils/auth";
+import CartItem from "../CartItem";
+import { useStoreContext } from "../../utils/GlobalState";
 import { useLazyQuery } from '@apollo/react-hooks';
 import { QUERY_CHECKOUT } from "../../utils/queries"
-import { idbPromise } from "../../utils/helpers"
-import CartItem from "../CartItem";
-import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 
